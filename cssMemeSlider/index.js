34610a5a669b0f00590f4832b50592d0687cbd28
Nikolai <nikolai.ivanov3110@gmail.com>
2023-09-14 19:22:55 +0300

    feat: implement slider logic (Thu, Sep 14, 2023 7:22:55 PM)

---
+++ cssMemeSlider/index.js
@@ -0,0 +1,48 @@
+const imagesContainer = document.querySelector('.slider');
+const controls = document.querySelectorAll('.control-bg');
+const outerButtons = document.querySelectorAll('.control-outer');
+const innerButtons = document.querySelectorAll('.control-inner');
+const image = document.querySelector('.meme');
+const memeContainer = document.querySelector('.meme-container');
+const texts = document.querySelectorAll('.meme-text');
+
+let currentIndex = 0;
+let slideWidth;
+
+
+function updateSlide() {
+    imagesContainer.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+    innerButtons.forEach((innerButton, index) => {
+        if(index === currentIndex) {
+            innerButton.style.backgroundColor = 'rgb(59, 59, 59)';
+        } else {
+            innerButton.style.backgroundColor = '';
+        }
+        texts.forEach((text, index) => {
+            if(index === currentIndex) {
+                // text.style.display = 'flex';
+                text.classList.add('appearance');
+            } else {
+                text.style.display = 'none';
+            }
+        })
+    })
+}
+
+controls.forEach((control, index) => {
+    control.addEventListener('click', () => {
+        console.log('click');
+        currentIndex = index;
+        updateSlide();
+    })
+});
+
+function updateSlideWidth() {
+    const imageWidth = image.width;
+    console.log(imageWidth);
+            slideWidth = imageWidth + 30;
+            updateSlide();
+        }
+        
+updateSlideWidth();
+window.addEventListener('resize', updateSlideWidth);
